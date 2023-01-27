059198731a61cc0b1d0294f73bd7d5731fb548ad
Max Orlov <indigoram89@ya.ru>
2023-01-27 15:44:49 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('octane', function (Request $request) {
-
-    $milliseconds = $request->input('sleep') ?: 0;
-
-    usleep($milliseconds * 1000);
-
-    return ['milliseconds' => $milliseconds];
+    $ms = $request->input('sleep') ?: 0;
+    usleep($ms * 1000);
+    return ['foo' => 'bar'];
 });
